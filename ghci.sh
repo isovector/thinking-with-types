3bd02945412108e2b980858a1bdb912cdc021050
Sandy Maguire <sandy@sandymaguire.me>
2020-03-07 03:00:55 +0700

    stop commenting out bad code

--- ghci.sh
+++ ghci.sh
@@ -4,6 +4,5 @@ response=$(mktemp /tmp/ghci-latex.XXXXXXXXXXX)
 
 # echo ":l $1" | cat - $2 | sed "s/^@//" | sed 's_^/[^/]\+/[^/]\+/__' | stack repl &> .latex-live-snippets/repl/$3.tex
 echo ":l $1" | cat - $2 | sed "s/^@//" | sed 's_^/[^/]\+/[^/]\+/__' | stack repl &> $response
-# latex-live-snippets-ghci $2 $response $3
 stack --stack-yaml=lib/latex-live-snippets/stack.yaml exec latex-live-snippets-ghci -- $2 $response $3
 
--- snip.sh
+++ snip.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 
-# latex-live-snippets code/$1.hs $2
 stack --stack-yaml=lib/latex-live-snippets/stack.yaml exec latex-live-snippets -- code/$1.hs $2
 sed -i "s/$2/$3/" ".latex-live-snippets/$1.$2.tex"
