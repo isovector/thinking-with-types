7583aced1c0f74dae21eeec449cf518f02cf739d
Sandy Maguire <sandy@sandymaguire.me>
2021-10-30 14:42:08 -0700

    Gitignore + remove more tex crap

--- ghci.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-response=$(mktemp /tmp/ghci-latex.XXXXXXXXXXX)
-
-# echo ":l $1" | cat - $2 | sed "s/^@//" | sed 's_^/[^/]\+/[^/]\+/__' | stack repl &> .latex-live-snippets/repl/$3.tex
-echo ":l $1" | cat - $2 | sed "s/^@//" | sed 's_^/[^/]\+/[^/]\+/__' | stack repl &> $response
-stack --stack-yaml=lib/latex-live-snippets/stack.yaml exec latex-live-snippets-ghci -- $2 $response $3
-
--- snip.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-stack --stack-yaml=lib/latex-live-snippets/stack.yaml exec latex-live-snippets -- code/$1.hs $2
-sed -i "s/$2/$3/" ".latex-live-snippets/$1.$2.tex"
